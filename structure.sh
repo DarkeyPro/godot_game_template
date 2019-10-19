e7cf1e5180ceaf4d5487c0c59a33dd58ef643136
DarkeyPro <darkey_pro@protonmail.com>
2019-10-19 16:00:35 +0000

    Add new file

---
+++ structure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mkdir Animation
+mkdir Notes
+mkdir Scenes
+mkdir Prefap
+mkdir Exports
+mkdir tests
+mkdir Database
+
+mkdir Material
+cd Material
+mkdir Shader
+
+cd ..
+
+mkdir Assets
+cd Assets
+mkdir Models
+mkdir Audio
+mkdir Texture
+mkdir Font
+
+cd ..
+
+mkdir Scripts
+cd Scripts
+mkdir Global
+mkdir Player
+mkdir Enemy
+mkdir GameSet
+
+cd ..
+mkdir Fx
+cd Fx
+mkdir Gpu
+mkdir Cpu
